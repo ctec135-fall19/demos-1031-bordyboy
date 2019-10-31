844b00bbc192c21875ed43f30db319d6d9b8ed58
Bogdan Livadaru <bogdanlivadaru@yahoo.com>
2019-10-31 15:15:28 -0700

    Class Demo

---
+++ Demo1031Spooky/MyMath1.UnitTests/MyMath1Tests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MyMath1;
+using NUnit.Framework;
+
+namespace MyMath1.UnitTests
+{
+    [TestFixture]
+    public class MyMath1Tests
+    {
+        [Test]
+        public void Add_SumWithinByteRange_ReturnCorrectSum()
+        {
+            Assert.AreEqual(200, MyMath.MyMath1.Add(101, 99));
+        }
+        [Test]
+        public void Add_SumOutsideByteRange_ReturnIncorrectSum()
+        {
+
+            Assert.AreEqual(302, MyMath.MyMath1.Add(101, 201));
+        }
+
+        [Test]
+        public void Add_SumWithinByteRange_AlternateAssert()
+        {
+            Assert.That(MyMath.MyMath1.Add(101, 99), Is.EqualTo(200));
+        }
+
+        [Test]
+
+        public void Add_SumsOutsideByteRange_ThrowsException()
+        {
+            Assert.That(() => MyMath.MyMath1.Add(101, 201), Throws.Exception);
+        }
+
+    }
+}
